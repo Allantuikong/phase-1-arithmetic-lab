4a4248a0ce278627c2248b2ce9ff22b9d07d3b1c
Allan Kipkorir <allantuix@gmail.com>
2024-03-29 22:47:51 +0300

    Completed Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
-
 // Write your code here
+
+//multiply
+
+// function multiply (num1, num2) {
+//     return num1 * num2;
+// }
+// console.log(multiply(31,2));
+
+// let multiply = 31 * 2;
+// console.log(multiply);
+
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+console.log(multiply);
+
+
+// Random
+
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+// mod
+
+let num3 = 24;
+let num4 =5;
+
+let mod =num3 % num4;
+console.log(mod);
+
+//max
+
+let max = Math.max(11,12,13,14,15,16,17,18,19,20);
+console.log(max);
